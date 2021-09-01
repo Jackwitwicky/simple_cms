42c6552cbc0fbe23c70bea4faf459559929c1e5a
Jackson Njiraini Kiarie <jkiarie@safaricom.co.ke>
2021-09-01 16:28:25 +0300

    ADD create page resource

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,28 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notive] = "Page created successfully"
+      redirect_to page_path(@page)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:id, :name, :permalink, :position, :visible, :subject_id)
   end
 end
