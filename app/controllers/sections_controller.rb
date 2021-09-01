16a1340408f5cc2831312fb0aeecf650fb49ec62
Jackson Njiraini Kiarie <jkiarie@safaricom.co.ke>
2021-09-01 17:38:24 +0300

    ADD create section resource

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -7,11 +7,29 @@ class SectionsController < ApplicationController
   end
 
   def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new(section_params)
+
+    if @section.save
+      flash[:notice] = 'Section created successfully'
+      redirect_to section_path @section
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:id, :name, :position, :visible, :content, :content_type, :page_id)
   end
 end
