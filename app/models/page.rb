0d49369f64e146ce42f8181b2f52e0f696a66841
Jackson Kiarie <jacknkiarie@gmail.com>
2021-09-20 10:03:05 +0300

    ADD: public controller to handle guest usage of the web app

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,11 @@ class Page < ApplicationRecord
   has_many :sections
   has_and_belongs_to_many :admin_users
 
-  scope :sorted, lambda { order(position: :asc) }
+      scope :visible, lambda { where(visible: true) }
+    scope :invisible, lambda { where(visible: false) }
+    scope :sorted, lambda { order(position: :asc) }
+    scope :newest_first, lambda { order(created_at: :desc) }
+    scope :search, lambda { |query| where(["name LIKE ?", "%#{query}"]) }
 
   validates_presence_of :name
   validates_length_of :name, maximum: 255
