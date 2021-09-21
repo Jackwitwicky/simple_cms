073125b10395a4fed64426a376c6732c3e8f7183
Jackson Kiarie <jacknkiarie@gmail.com>
2021-09-21 10:09:10 +0300

    REFACTOR: sections to nest in pages

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,10 +3,11 @@ class SectionsController < ApplicationController
   layout 'admin'
 
   before_action :confirm_logged_in
+  before_action :find_page
   before_action :set_section_count, only: [:new, :create, :edit, :update]
 
   def index
-    @sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def show
@@ -14,15 +15,16 @@ class SectionsController < ApplicationController
   end
 
   def new
-    @section = Section.new
+    @section = Section.new(page_id: @page.id)
   end
 
   def create
     @section = Section.new(section_params)
+    @section.page = @page
 
     if @section.save
       flash[:notice] = 'Section created successfully'
-      redirect_to section_path @section
+      redirect_to section_path(@section, page_id: @page.id)
     else
       render 'new'
     end
@@ -37,7 +39,7 @@ class SectionsController < ApplicationController
 
     if @section.update(section_params)
       flash[:notice] = 'Section updated successfully'
-      redirect_to sections_path
+      redirect_to sections_path, page_id: @page.id
     else
       render 'edit'
     end
@@ -51,13 +53,17 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     @section.destroy
     flash[:notice] = 'Section deleted successfully'
-    redirect_to sections_path
+    redirect_to sections_path, page_id: @page.id
   end
 
   private
 
+  def find_page
+    @page = Page.find(params[:page_id])
+  end
+
   def section_params
-    params.require(:section).permit(:id, :name, :position, :visible, :content, :content_type, :page_id)
+    params.require(:section).permit(:id, :name, :position, :visible, :content, :content_type)
   end
 
   def set_section_count
